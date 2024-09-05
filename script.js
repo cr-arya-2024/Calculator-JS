3f9dd2c239624a6e16ff5bd075bf2396ad2a27c5
cr-arya-2024 <crarya2024@gmail.com>
2024-09-05 18:29:18 +0530

    adding html,css,js files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const input=document.getElementById("input")
+const equalsTo=document.getElementById("equalsTo")
+function toWrite(ele){
+    
+    input.value+=ele.id
+}
+function negativeOperator(ele){
+    
+    input.value=-input.value
+}
+function equalTo(){
+    const output=eval(input.value)
+input.value=output
+   
+}
+function toCancel(){
+    input.value=""
+}
